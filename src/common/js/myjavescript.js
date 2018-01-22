849fec77f54325cfc37b79ef4c613a88f14255e6
wyyyy <xfxbnb@163.com>
2018-01-21 21:48:44 -0800

    new

--- src/common/js/myjavescript.js
+++ src/common/js/myjavescript.js
@@ -9,17 +9,29 @@
         new Vue({
             el: '#box',
             data: datalist,
+            created: function () {
+                console.log("created-:实例已经创建" + (new Date()));
+            },
+            beforeCompile: function () {
+                console.log("beforeCompile:编译之前" + (new Date()));
+            },
+            compiled: function () {
+                console.log("compiled-:编译之后");
+            },
+            ready: function () {
+                console.log("ready-:插入到文档中");
+            },
             methods: {
                 add: function () {
                     console.log(this.arrs);
                 },
-                mKeydown:function(ev,index){
-                    if (index==1) {
-                    console.log("mKeydown-:keyCode"+ev.keyCode);
-                        
+                mKeydown: function (ev, index) {
+                    if (index == 1) {
+                        console.log("mKeydown-:keyCode" + ev.keyCode);
+
                     } else {
-                    console.log("keyup-:keyCode"+ev.keyCode);
-                        
+                        console.log("keyup-:keyCode" + ev.keyCode);
+
                     }
                 }
             }
